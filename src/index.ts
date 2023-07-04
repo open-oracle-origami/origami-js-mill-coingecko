954a49dab37b977a91c9aef46f65c667ed1d562c
Gorilla's Habitat <gorilla@creaselabs.io>
2023-07-04 11:07:49 -0700

    feat: pushing initial version

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { CoinGeckoClient } from 'coingecko-api-v3'
+
+import { BaseMill } from '@open-oracle-origami/origami-js-sdk'
+import { poll } from 'poll'
+
+type SimplePriceParams = {
+  vs_currencies: string
+  ids: string
+  include_market_cap?: boolean
+  include_24hr_vol?: boolean
+  include_24hr_change?: boolean
+  include_last_updated_at?: boolean
+}
+
+export class CoinGeckoMill extends BaseMill {
+  private readonly client: CoinGeckoClient
+  private readonly pollIntervalMs: number
+  simplePriceParams: SimplePriceParams
+
+  constructor({
+    id = 'coingecko',
+    // @ts-ignore
+    simplePriceParams,
+    pollIntervalMs = 5000,
+  }) {
+    super()
+
+    if (id) this.setId(id)
+    this.pollIntervalMs = pollIntervalMs
+    this.simplePriceParams = simplePriceParams
+    this.client = new CoinGeckoClient({
+      timeout: 10000,
+      autoRetry: true,
+    })
+  }
+
+  private getSimplePrice = async () => {
+    return this.client.simplePrice(this.simplePriceParams).then((data: any) => {
+      const paper = {
+        data,
+        created: new Date(),
+      }
+
+      this.emitter.publish(`mill.${this.id ?? 'undefined'}`, paper)
+    })
+  }
+
+  start = () => {
+    super.start()
+
+    if (!this.simplePriceParams) {
+      throw new Error('simplePriceParams is required in constructor')
+    }
+
+    void poll(this.getSimplePrice, this.pollIntervalMs)
+  }
+}
+
+export default CoinGeckoMill
